c16bd5924369341330adc32d1be53260052210fb
Pawel Grobelniak <pawel.grobelniak@gmail.com>
2025-01-23 15:31:09 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 rm -f build.bas
 i=10
 
-while read line; do
-	line=$(echo $line | awk '{$1=$1};1')
-	if [[ $line == "" ]]; then continue; fi
-	if echo $line | grep "^rem"; then continue; fi
+while read -r line; do
+	line=$(echo "$line" | awk '{$1=$1};1')
+	if [[ "$line" == "" ]]; then continue; fi
+	if echo "$line" | grep "^rem"; then continue; fi
 	echo "$i $line" >> build.bas
 	i=$((i+10))
 done < mutineers.bas
