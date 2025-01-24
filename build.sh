8af3cab8f8eeba4bde246b867a48a679f7ea0018
Pawel Grobelniak <pawel.grobelniak@gmail.com>
2025-01-24 09:44:37 +0100

    fix build.sh - do not squeeze spaces

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -f build.bas
 i=10
 
 while read -r line; do
-	line=$(echo "$line" | awk '{$1=$1};1')
+	line=$(echo "$line" | sed 's/^[ \t]*//;s/[ \t]*$//')
 	if [[ "$line" == "" ]]; then continue; fi
 	if echo "$line" | grep "^rem"; then continue; fi
 	echo "$i $line" >> build.bas
